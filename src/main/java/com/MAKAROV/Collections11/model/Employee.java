a27055d7d75362b66b332ff37a7b856950e608cb
Makarov <infernochetac31@gmail.com>
2023-05-10 02:07:04 +0300

    done

--- src/main/java/com/MAKAROV/Collections11/model/Employee.java
+++ src/main/java/com/MAKAROV/Collections11/model/Employee.java
@@ -5,18 +5,19 @@ import java.util.Objects;
 public class Employee {
     private String firstName;
     private String lastName;
+    private final int id;
+    private String fio;
+    private int salary;
+    private int department;
 
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
 
-    public Employee(String firstName, String lastName) {
+    public Employee(String firstName, String lastName, int id, String fio, int salary, int department) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.id = id;
+        this.fio = fio;
+        this.salary = salary;
+        this.department = department;
     }
 
     public String getFirstName() {
@@ -35,16 +36,56 @@ public class Employee {
         this.lastName = lastName;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+        return id == employee.id && salary == employee.salary && department == employee.department && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(fio, employee.fio);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(firstName, lastName);
+        return Objects.hash(firstName, lastName, id, fio, salary, department);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                ", fio='" + fio + '\'' +
+                ", salary=" + salary +
+                ", department=" + department +
+                '}';
     }
 }
--- src/main/java/com/MAKAROV/Collections11/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/MAKAROV/Collections11/service/impl/EmployeeServiceImpl.java
@@ -2,12 +2,54 @@ package com.MAKAROV.Collections11.service.impl;
 
 import com.MAKAROV.Collections11.exception.EmployeeAlreadyAddedException;
 import com.MAKAROV.Collections11.exception.EmployeeNotFoundException;
-import com.MAKAROV.Collections11.exception.EmployeeStorageIsFullException;
 import com.MAKAROV.Collections11.model.Employee;
 import com.MAKAROV.Collections11.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private final List<Employee> employees;
+    public EmployeeServiceImpl(){
+        employees = new ArrayList<>();
+    }
+    @Override
+    public Employee addEmployee(String firstName, String lastName, Integer id, String fio, Integer salary, Integer department) {
+        Employee employee = new Employee(firstName, lastName, id, fio,salary, department);
+        if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+        employees.add(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee removeEmployee(String firstName, String lastName, Integer id, String fio, Integer salary, Integer department) {
+        Employee employee = new Employee(firstName, lastName, id, fio,salary, department);
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+        employees.remove(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee findEmployee(String firstName, String lastName, Integer id, String fio, Integer salary, Integer department) {
+        Employee findEmployee = new Employee(firstName, lastName, id, fio,salary, department);
+        for (Employee employee : employees) {
+            if (employee.equals(findEmployee)) {
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAllEmployee() {
+        return employees;
+    }
 
 }
